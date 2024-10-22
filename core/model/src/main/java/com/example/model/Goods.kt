45dbc4b574cd44393d1c9c0fe7894d09cbaa7ce4
kameryo <69069853+kameryo@users.noreply.github.com>
2024-10-22 11:25:27 +0900

    add goods model

---
+++ core/model/src/main/java/com/example/model/Goods.kt
@@ -0,0 +1,13 @@
+package com.example.model
+
+data class Goods(
+    val id: Long,
+    val name: String,
+    val price: Int,
+    val purchases: Int,
+    val remain: Int,
+    val isAvailable: Boolean,
+)
+
+
+
