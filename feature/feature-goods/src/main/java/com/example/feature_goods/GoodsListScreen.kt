3c5d922ad640a9851d2efd747f672342bd37d560
kameryo <69069853+kameryo@users.noreply.github.com>
2024-10-31 11:04:30 +0900

    Fix warning

--- feature/feature-goods/src/main/java/com/example/feature_goods/GoodsListScreen.kt
+++ feature/feature-goods/src/main/java/com/example/feature_goods/GoodsListScreen.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
@@ -50,7 +50,7 @@ private fun GoodsScreen(
                 title = { Text(stringResource(R.string.goods_manage)) },
                 navigationIcon = {
                     IconButton(onClick = back) {
-                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
                     }
                 },
                 actions = {
@@ -99,6 +99,6 @@ fun GoodsListItem(
             onClick()
         }
     )
-    Divider()
+    HorizontalDivider()
 
 }
